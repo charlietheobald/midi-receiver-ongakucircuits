d3935adffb2018736371e2d155ee422cd1760f71
charlietheobald <charlie.theobald.home@gmail.com>
2026-07-13 16:49:34 +0100

    Updated note on / note off handles with fixed size polyphonic buffer

--- src/main.cpp
+++ src/main.cpp
@@ -1,187 +1,309 @@
-// To fix:
-// Running Status needs to be implemented separately for every MIDI channel
 
 #include <Arduino.h>
+#include <MIDI.h>
+#include <deque>
+#include <utility>
+#include <string>
 
-#define MIDI_IN_PIN 4
+// Replace with hardware serial if possible
+// SoftwareSerial midiSerial(4, 3); // RX = pin 4, TX = pin 3
+#define MIDI_IN_PIN = 4;
+MIDI_CREATE_INSTANCE(HardwareSerial, Serial1, MIDI);
 
-struct midiEvent{
-  uint8_t status1;
-  uint8_t param1;
-  uint8_t param2;
+struct Voice{
+    int pitch;
+    int octave;
+    bool free = true;
 };
 
-// MIDI MESSAGE RESPONSE STATE TRANSITIONS
+const int iPOLYPHONY_BUFFER_SIZE = 6;
 
-// nb it might be better to put everything into a single enum that way we have just one status to detect
-// type1 for two-parameter messages
-// in either Normal or Running Status mode
-// Example commands are Note On / Note Off / Polyphonic Key Pressure / Control Change / Channel Mode
+Voice voices[iPOLYPHONY_BUFFER_SIZE]; // creates a fixed-length array of length iPOLYPHONY_BUFFER_SIZE
+std::deque<int> dqiNotePriority;
 
-// type2 for one-parameter messages
-// in either Normal or Running Status mode
-// Example commands are Program Change / Channel Pressure
+std::pair<std::string, int> psiMIDINotetoSPN(const int MIDInote){
+    int iScaleIndex = MIDInote % 12;
+    int iOctaveIndex = MIDInote / 12; // integer division
+    std::string pitchName;
+    int octave = iOctaveIndex - 1; // This can be shown to be true (assuming that iOctaveIndex is a quotient. Look at the MIDI table)
+    // C has remainder 0
+    // Which means C# has remainder 1.... etc
+    switch(iScaleIndex){
+        case 0:
+            pitchName = "C";
+            break;
+        case 1:
+            pitchName = "C#";
+            break;
+        case 2:
+            pitchName = "D";
+            break;
+        case 3:
+            pitchName = "D#";
+            break;
+        case 4:
+            pitchName = "E";
+            break;
+        case 5:
+            pitchName = "F";
+            break;
+        case 6:
+            pitchName = "F#";
+            break;
+        case 7:
+            pitchName = "G";
+            break;
+        case 8:
+            pitchName = "G#";
+            break;
+        case 9:
+            pitchName = "A";
+            break;
+        case 10:
+            pitchName = "A#";
+            break;
+        case 11:
+            pitchName = "B";
+            break;
+        default:
+            pitchName = "Z";
+            break;
+    }
+    return std::make_pair(pitchName, octave);
+}
 
 
-// type3 for messages without parameters
-// irrelevant of Normal or Running Status
-// Example commands are Tune Request / Timing Clock / Active Sensing / Reset
+int iFindIndexInDeque(int ipitchNum){
+    // Search the note-priority deque for the given MIDI pitch and return its index
+    for(int i = 0; i < dqiNotePriority.size(); i++){
+        if(dqiNotePriority[i] == ipitchNum){
+            return i;
+        }
+    }
+    return -1;
+}
 
-enum midiReceiverState{
-  MIDI_STATUS, CHANNEL, VELOCITY, PARAMETER, RUNNINGSTATUSCHECK
-};
+int findFreeVoice(){
+    for(int i = 0; i < iPOLYPHONY_BUFFER_SIZE; i++){
+        if(voices[i].free){
+            return i;
+        }
+    }
+    return -1;
+}
 
-int messageType;
-uint8_t lastStatus;
+int findVoicefromPitch(int pitch){
+    for(int i = 0; i < iPOLYPHONY_BUFFER_SIZE; i++){
+        if(voices[i].pitch == pitch){
+            return i;
+        }
+    }
+    return -1;
+}
+
+void handleNoteOn(byte channel, byte pitch, byte velocity){
+    Serial.print("Note On  | Channel: ");
+    Serial.print(channel);
+    Serial.print(" | Pitch: ");
+    Serial.print(pitch);
+    Serial.print(" | Velocity: ");
+    Serial.println(velocity);
+    Serial.print(" | Assigned voice: ");
+    // Look for a free voice
+    int iNextFreeVoice = findFreeVoice();
+    if(iNextFreeVoice != -1){ // If a free voice is found
+        voices[iNextFreeVoice].pitch = pitch;
+        voices[iNextFreeVoice].octave = pitch / 12;
+        voices[iNextFreeVoice].free = false;
+        //voices[iNextFreeVoice].velocity = velocity;
+        dqiNotePriority.push_back(pitch);
+        Serial.println(iNextFreeVoice);
+    }
+    else{ // If no free voices are found, remove the oldest voice
+
+        // Get the element at the front of dqiNotePriority
+        int iOldestVoice_pitch = dqiNotePriority.front();
+        // Find the index of the voice with this pitch
+        int iOldestVoice_index = findVoicefromPitch(iOldestVoice_pitch);
+        // Remove all data
+        // And rewrite with the new note
+        if(iOldestVoice_index != -1){
+            dqiNotePriority.pop_front();
+            voices[iOldestVoice_index].pitch = pitch;
+            voices[iOldestVoice_index].octave = pitch / 12;
+            voices[iOldestVoice_index].free = false;
+            //voices[iOldestVoice_index].velocity = velocity;
+            dqiNotePriority.push_back(pitch);
+            Serial.println(iOldestVoice_index);
+        }
+        else{
+            Serial.println("Tried to overwrite a note, but its index cannot be found.");
+        }
+
+    }
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity){
+    Serial.print("Note Off | Channel: ");
+    Serial.print(channel);
+    Serial.print(" | Pitch: ");
+    Serial.print(pitch);
+    Serial.print(" | Velocity: ");
+    Serial.println(velocity);
+    // Look for switched off note's pitch in the running voices
+    int iSwitchedOffVoice = findVoicefromPitch(pitch);
+    // Mark note as free in the array and remove from deque if present
+    voices[iSwitchedOffVoice].free = true;
+    // Find the DEQUE index of the note to be removed
+
+    int iSwitchedOffVoiceIndex_deque = iFindIndexInDeque(pitch);
+    dqiNotePriority.erase(dqiNotePriority.begin() + iSwitchedOffVoiceIndex_deque);
+}
+/*
+uint16_t uCreateNoteBuffer(std::deque<std::string> const &dqsNoteFIFO){
+    uint16_t outputNoteBuffer = 0;
+    for(int i = 0; i < dqsNoteFIFO.size(); i++){
+        if(dqsNoteFIFO[i] == "C"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 11);
+        }
+        else if(dqsNoteFIFO[i] == "C#"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 10);
+        }
+        else if(dqsNoteFIFO[i] == "D"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 9);
+        }
+        else if(dqsNoteFIFO[i] == "D#"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 8);
+        }
+        else if(dqsNoteFIFO[i] == "E"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 7);
+        }
+        else if(dqsNoteFIFO[i] == "F"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 6);
+        }
+        else if(dqsNoteFIFO[i] == "F#"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 5);
+        }
+        else if(dqsNoteFIFO[i] == "G"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 4);
+        }
+        else if(dqsNoteFIFO[i] == "G#"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 3);
+        }
+        else if(dqsNoteFIFO[i] == "A"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 2);
+        }
+        else if(dqsNoteFIFO[i] == "A#"){
+            outputNoteBuffer = outputNoteBuffer | (1 << 1);
+        }
+        else if(dqsNoteFIFO[i] == "B"){
+            outputNoteBuffer = outputNoteBuffer | 1;
+        }
+        else{
+            outputNoteBuffer = 0;
+            Serial.print("WARNING Buffer contains at least one malformed or undefined pitch");
+        }
+    }
+    return outputNoteBuffer;
+}
 
 
+// We need to have three separate deques:
+// dqiMIDINoteFIFO is needed to find unique MIDI notes in the input array
+// which cannot be implemented with either of the two FIFOs as they are octave and pitch blind respectively
+std::deque<int> dqiMIDINoteFIFO;
+std::deque<std::string> dqsNoteFIFO;
+std::deque<uint16_t> dqiOctaveFIFO;
 
+void handleNoteOn(byte channel, byte pitch, byte velocity) {
+    // If there are less than 6 notes in the buffer: add to the buffer
+    if(dqiMIDINoteFIFO.size() < iPOLYPHONY_BUFFER_SIZE){
+        dqiMIDINoteFIFO.push_back(pitch);
+        dqsNoteFIFO.push_back(psiMIDINotetoSPN(pitch).first);
+        dqiOctaveFIFO.push_back(psiMIDINotetoSPN(pitch).second);
+    }
+    // If the buffer is full: dynamically remove notes from it
+    // and re-add new ones if they are being played
+    else if(dqiMIDINoteFIFO.size() >= iPOLYPHONY_BUFFER_SIZE){
+        Serial.println("Polyphony limit reached");
+        dqiMIDINoteFIFO.pop_front();
+        dqiMIDINoteFIFO.push_back(pitch);
+
+        dqsNoteFIFO.pop_front();
+        dqsNoteFIFO.push_back(psiMIDINotetoSPN(pitch).first);
+
+        // Prevent clicking:
+        // Need a new data structure (maybe an array?)
+        // Memory: dqsPreviousNoteFIFO, dqsCurrentNoteFIFO
+        // Any elements that are the same in Previous and Current
+        // Should not have their position changed in the output array
+
+        // Implementation: Sort all elements in alphabetical order
+        // 
+
+        // Or the "free voices" idea?
+
+
+        dqiOctaveFIFO.pop_front();
+        dqiOctaveFIFO.push_back(psiMIDINotetoSPN(pitch).second);
+    }
+
+    uint16_t noteBuffer = uCreateNoteBuffer(dqsNoteFIFO);
+
+    Serial.print("Note On  | Channel: ");
+    Serial.print(channel);
+    Serial.print(" | Pitch: ");
+    Serial.print(pitch);
+    Serial.print(" | Velocity: ");
+    Serial.println(velocity);
+}
+
+void handleNoteOff(byte channel, byte pitch, byte velocity){
+
+    // Remove the note with Note Off detected from all the buffers
+    // Notes dynamically removed from the polyphony buffer as it becomes full will not reach this point
+    int iIndexInBuffer = iFindIndexInDeque(pitch, dqiMIDINoteFIFO);
+    if(iIndexInBuffer != -1){
+        dqiMIDINoteFIFO.erase(dqiMIDINoteFIFO.begin() + iIndexInBuffer);
+        dqsNoteFIFO.erase(dqsNoteFIFO.begin() + iIndexInBuffer);
+        dqiOctaveFIFO.erase(dqsNoteFIFO.begin() + iIndexInBuffer);
+    }
+    else{
+        Serial.println("An attempt was made to turn off a note that was automatically removed by the polyphony FIFO. Safely continuing...");
+    }
+
+    uint16_t noteBuffer = uCreateNoteBuffer(dqsNoteFIFO);
+    Serial.print("Note Off | Channel: ");
+    Serial.print(channel);
+    Serial.print(" | Pitch: ");
+    Serial.print(pitch);
+    Serial.print(" | Velocity: ");
+    Serial.println(velocity);
+}
+*/
 
 void setup() {
-  Serial.begin(115200);
-  Serial1.begin(31250, SERIAL_8N1, MIDI_IN_PIN, -1, true); 
-  delay(100);
-  while (Serial1.available()) Serial1.read();
+    Serial.begin(115200);
+    Serial1.begin(31250, SERIAL_8N1, MIDI_IN_PIN, -1, false);
+    while(Serial1.available()){
+        Serial1.read();
+    }
+    delay(500);
+    Serial.println("MIDI Receiver Initializing...");
 
-  Serial.println("Ready"); 
+    MIDI.begin(MIDI_CHANNEL_OMNI);
+    MIDI.turnThruOff();
+    MIDI.setHandleNoteOn(handleNoteOn);
+    MIDI.setHandleNoteOff(handleNoteOff);
+
+    Serial.println("MIDI Receiver Ready!");
 }
-
-
 
 void loop() {
-  // Use a while loop to instantly empty the hardware buffer without delays
-  while (Serial1.available() > 0) {
-    uint8_t midiData = Serial1.read();
+    MIDI.read();
     
-    static midiReceiverState currentState;
-    static midiReceiverState nextState;
-    static midiEvent midievent;
-
-    // Print a leading zero for single-digit hex values (e.g., 02 instead of 2)
-    if (midiData < 0x10){
-      Serial.print("0");
-    }
-    Serial.print(midiData, HEX);
-    Serial.print(" ");
-
-
-    uint8_t midiData_top4 = midiData >> 4; // the top 4 bits of midiData, representing the status message
-    uint8_t midiData_bottom4 = midiData & (0x0F); // the bottom 4 bits of midiData, representing the MIDI chnannel
-    bool realTime = false; // realTime init to false. Used to allow running status to continue even with a "1" as MSB in a Real-Time Message
-
-    if(midiData >> 7 == 1 && currentState != RUNNINGSTATUSCHECK){
-      currentState = MIDI_STATUS;
-    }
-    // else we are receiving data of the same type as before
-
-    switch(currentState){
-      case(MIDI_STATUS):
-        // If we're in one of the STATUS states
-        // Record the event type in latestStatus
-        // nextState always a channel (type1) or a parameter (type2) or another status (type3)
-        switch(midiData_top4){
-          case(0x9): // Note On
-            messageType = 1;
-            nextState = CHANNEL;
-            break;
-          case(0x8): // Note Off
-            messageType = 1;
-            nextState = CHANNEL;
-            break;
-          case(0xA): // Polyphonic Key Pressure
-            messageType = 1;
-            nextState = CHANNEL;
-            break;
-          case(0xB): // Control Change / Channel Mode depending on the channel bits
-            messageType = 1;
-            nextState = CHANNEL;
-            break;
-          case(0xE): // Pitch Bend Change
-            messageType = 1;
-            nextState = CHANNEL;
-            break;
-      
-
-          case(0xC): // Program Change
-            messageType = 2;
-            nextState = PARAMETER;
-            break;
-          case(0xD): // Channel Pressure
-            messageType = 2;
-            nextState = PARAMETER;
-            break;
-
-          case(0xF):
-            // This is the bunch of weird type3 messages
-            switch(midiData_bottom4){
-              // System Common Messages - ignored for now
-
-              // Real-Time Messages
-              case(0x8): // Timing Clock
-                break;
-              case(0x9): // Undefined
-                break;
-              case(0xA): // Start
-                break;
-              case(0xB): // Continue
-                break;
-              case(0xC): // Stop
-                break;
-              case(0xD): // Undefined
-                break;
-              case(0xE): // Active Sensing (connection alive every 300ms)
-                break;
-              case(0xF): // Reset
-                break;
-            }
-            realTime = true;
-            messageType = 3;
-            nextState = MIDI_STATUS;
-          
-        }
-        lastStatus = midiData;
-        break;
-      case(CHANNEL):
-        nextState = VELOCITY;
-        break;
-      case(VELOCITY):
-        nextState = RUNNINGSTATUSCHECK;
-        break;
-      case(PARAMETER):
-        nextState = RUNNINGSTATUSCHECK;
-        break;
-      case(RUNNINGSTATUSCHECK):
-
-        // if a 1 is detected we are not in Running Status
-        // FIX: Real-Time Messages should not affect Running Status.
-        if(midiData >> 7 == 1 && !realTime){
-          nextState = MIDI_STATUS;
-        }
-
-        // Running Status triggered
-        else{
-          if(messageType == 1){
-            nextState = CHANNEL;
-          }
-          else if(messageType == 2){
-            nextState = PARAMETER;
-          }
-          else if(messageType == 3){
-            nextState = MIDI_STATUS;
-          }
-        break;
-      }
-    }
-
-
-
-    // Other logic
-
-    // FIX: additional logic required to regroup Running Status with data bytes
-    currentState = nextState;
-    midievent.status1 = lastStatus;
-    //midievent.param1 = channel;
-    //midievent.param2 = velocity;
-  }
+    // OUTPUTS:
+    // Six active TOG selects
+    // Will need a separate function to encode the TOG selects
+    // Six divider signals
 }
-
